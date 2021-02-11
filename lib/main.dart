ac8f7febffa19bc3a5be74f77ed3063b4d205325
DianaM9031 <diana.moron@outlook.com>
2021-02-11 14:20:35 -0500

    added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:bloc_example/bloc/weather_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'model/weather.dart';
+import 'pages/weather_search_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,134 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: WeatherPage(),
+      title: 'Material App',
+      home: WeatherSearchPage(),
     );
-  }
-}
-
-class WeatherPage extends StatefulWidget {
-  WeatherPage({Key key}) : super(key: key);
-
-  @override
-  _WeatherPageState createState() => _WeatherPageState();
-}
-
-class _WeatherPageState extends State<WeatherPage> {
-  final weatherBloc = WeatherBloc(WeatherInitial());
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Fake Weather App"),
-      ),
-      body: BlocProvider(
-        create: (context) => weatherBloc,
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 16),
-          alignment: Alignment.center,
-          child: BlocListener<WeatherBloc, WeatherState>(
-            listener: (context, state) {
-              if(state is WeatherLoaded) {
-                print('Loaded: ${state.weather.cityName}');
-              }
-            },
-            child: BlocBuilder<WeatherBloc, WeatherState>(
-                builder: (context, state) {
-                  if(state is WeatherInitial) {
-                    return buildInitialInput();
-                  } else if(state is WeatherLoading) {
-                    return buildLoading();
-                  } else if(state is WeatherLoaded) {
-                    return buildColumnWithData(state.weather);
-                  } else {
-                    return Container();
-                  }
-                },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Column buildColumnWithData(Weather weather) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Text(
-          '${weather.cityName}',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        Text(
-          "${weather.temperature.toStringAsFixed(1)} °C",
-          style: TextStyle(fontSize: 80),
-        ),
-        CityInputField(),
-      ],
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    // weatherBloc.dispose();
-  }
-
-  Widget buildInitialInput() {
-    return Center(
-      child: CityInputField(),
-    );
-  }
-
-  Widget buildLoading() {
-    return Center(
-      child: CircularProgressIndicator(),
-    );
-  }
-}
-
-class CityInputField extends StatefulWidget {
-  const CityInputField({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _CityInputFieldState createState() => _CityInputFieldState();
-}
-
-class _CityInputFieldState extends State<CityInputField> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 50),
-      child: TextField(
-        onSubmitted: submitCityName,
-        textInputAction: TextInputAction.search,
-        decoration: InputDecoration(
-          hintText: "Enter a city",
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-          suffixIcon: Icon(Icons.search),
-        ),
-      ),
-    );
-  }
-
-  void submitCityName(String cityName) {
-    final weatherBloc = BlocProvider.of<WeatherBloc>(context);
-    weatherBloc.add(GetWeather(cityName));
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 }
