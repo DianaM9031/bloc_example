f12852d4eb26eb4c8948dfa8b9f01f21a299823d
DianaM9031 <diana.moron@outlook.com>
2021-02-11 09:28:09 -0500

    test

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,35 +1,13 @@
-import 'dart:async';
-
+import 'package:bloc/bloc.dart';
 import 'counter_event.dart';
+import 'counter_state.dart';
 
-class CounterBloc {
-  int _counter = 0;
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc(CounterState initialState) : super(initialState);
 
-  final _counterStateController = StreamController<int>();
-  StreamSink<int> get _inCounter => _counterStateController.sink;
-  // For state, exposing only a stream which outputs data
-  Stream<int> get counter => _counterStateController.stream;
-
-  final _counterEventController = StreamController<CounterEvent>();
-  // For events, exposing only a sink which is an input
-  Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
-
-  CounterBloc() {
-    // Whenever there is a new event, we want to map it to a new state
-    _counterEventController.stream.listen(_mapEventToState);
-  }
-
-  void _mapEventToState(CounterEvent event) {
-    if (event is IncrementEvent)
-      _counter++;
-    else
-      _counter--;
-
-    _inCounter.add(_counter);
-  }
-
-  void dispose() {
-    _counterStateController.close();
-    _counterEventController.close();
+  @override
+  Stream<CounterState> mapEventToState(CounterEvent event) {
+    // TODO: implement mapEventToState
+    throw UnimplementedError();
   }
 }
